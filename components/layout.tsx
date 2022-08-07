7afcaa3544630c70d6a82d41d488da22f4a37377
okdyy75 <okdyy75@gmail.com>
2022-08-07 17:49:27 +0900

    ogpとfavicon設定

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,39 @@
 import Head from 'next/head'
 import { Container, Box, Typography, Link } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const Layout = ({ children }: any) => {
+    const { asPath } = useRouter()
+
     const title = 'ゆーキャプ'
     const description = '複数のURLからまとめてキャプチャ取得ツール'
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+    const ogImageUrl = baseUrl + '/images/og_image.jpg'
+    const iconUrl = baseUrl + '/images/icon.jpg'
+    const canonicalUrl = baseUrl + asPath
+
     return (
         <>
             <Head>
                 <title>{title}</title>
-                <meta name="description" content={description} />
+                <meta name="description" content={description}></meta>
+                <meta name="thumbnail" content={ogImageUrl}></meta>
+                <meta property="og:locale" content="ja_JP"></meta>
+                <meta property="og:title" content={title}></meta>
+                <meta property="og:description" content={description}></meta>
+                <meta property="og:image" content={ogImageUrl}></meta>
+                <meta property="og:image:width" content="1200"></meta>
+                <meta property="og:image:height" content="630"></meta>
+                <meta property="og:url" content={canonicalUrl}></meta>
+                <meta property="og:site_name" content={title}></meta>
+                <meta name="twitter:title" content={title}></meta>
+                <meta name="twitter:description" content={description}></meta>
+                <meta name="twitter:image" content={ogImageUrl}></meta>
+                <meta name="twitter:card" content="summary_large_image"></meta>
+                <link rel="apple-touch-icon" href={iconUrl}></link>
+                <link rel="apple-touch-icon" sizes="180x180" href={iconUrl}></link>
+                <meta name="msapplication-TileImage" content={iconUrl}></meta>
+                <link rel="canonical" href={canonicalUrl}></link>
             </Head>
             <Container maxWidth="md" sx={{ mt: 4 }}>
                 <Box component="footer">
