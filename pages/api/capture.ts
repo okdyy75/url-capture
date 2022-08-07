12c58e60b26bc7bd8c59aa62441e2b5cd14275c2
okdyy75 <okdyy75@gmail.com>
2022-08-07 18:01:35 +0900

    適当なユーザーエージェントを設定しておく（見れない場合があるので）

--- pages/api/capture.ts
+++ pages/api/capture.ts
@@ -63,6 +63,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     let page = await browser.newPage()
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36')
 
     // スクリーンショット取得
     const imageType = 'png'
